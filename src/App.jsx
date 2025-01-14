80a35e6d861d5bc01ddf64b143447d47998db0c2
Rodrigo Cajal <rodrigoecajal@gmial.com>
2025-01-14 19:47:39 -0300

    route

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,14 @@ import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
 import Layout from "./components/layout/layout";
 import { Routes } from "../src/components/routes/routes";
-import { BrowserRouter } from "react-router-dom"; 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Navbar />
-        <Routes />
-        <Footer />
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <Navbar />
+      <Routes /> 
+      <Footer />
+    </Layout>
   );
 }
 
